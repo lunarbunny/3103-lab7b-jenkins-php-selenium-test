34557299b997add1a4f317f970e3dfce1192f60e
lunarbunny <tingwei1734@gmail.com>
2023-11-09 00:41:37 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
+docker container stop my-apache-php-app || true
+docker container rm my-apache-php-app || true
 docker run -d -p 80:80 --name my-apache-php-app -v ~/Downloads/jenkins-php-selenium-test:/var/www/html php:7.2-apache
 sleep 1
 set +x
